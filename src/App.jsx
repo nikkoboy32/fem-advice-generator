57e350bdd8f1589b93bd9b0bef3cf660fe3ca2fe
Nikko <117736564+nikkoboy32@users.noreply.github.com>
2024-09-09 21:28:57 +0800

    all files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Advice from "./components/Advice"
+
+function App() {
+  
+
+  return (
+    <div className="app_container">
+      <Advice/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Advice.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react"
+
+export default function Advice() {
+
+    const [advice, setAdvice] = useState(null)
+    const [loading, setLoading] = useState(true);
+
+
+    const fetchAdvice = () => {
+        setLoading(true)
+        fetch(`https://api.adviceslip.com/advice?${new Date().getTime()}`)
+        .then(res => res.json())
+        .then(data => {
+            setAdvice(data.slip)
+            setLoading(false)
+        })
+    }
+
+    useEffect(()=> {
+        fetchAdvice() 
+    },[])
+
+    function handleClick() {
+        fetchAdvice()
+    }
+
+    if (loading) {
+        return <p className="loading">Loading...</p>;
+    }
+   
+
+   console.log(advice)
+
+    return (
+        <div className="advice_container">
+            <p>ADVICE #117</p>
+           <div className="advice">
+              <h2>"{advice.advice}"</h2>
+           </div>
+           <div className="btn_container" onClick={handleClick}>
+             <button className="generate_btn"><img src="/images/icon-dice.svg" alt="" /></button>
+           </div>
+        </div>
+    )
+}
